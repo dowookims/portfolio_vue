87dbc67927659b85812a902b0f6572a8a076a324
KimDoWoo <kaei3546@naver.com>
2019-10-15 13:42:52 +0900

    [Init]

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,16 +3,19 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'portfolio_vue',
+    title: 'KIM DO WOO',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: `Kim Dowoo's Nuxt.js project for portfolio` }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  modules: [
+    '@nuxtjs/axios'
+  ],
   /*
   ** Customize the progress bar color
   */
