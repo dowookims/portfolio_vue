27f13462116caed48daf8df287d09915e5892d23
dowookims <kaei2574@gmail.com>
2019-10-17 23:00:00 +0900

    [ADD] MouseWheel event to control project div

--- pages/project.vue
+++ pages/project.vue
@@ -1,26 +1,30 @@
 <template>
-  <section class="container">
-    <div class="project--div" ref="scrollDiv">
+  <section class="container" @mousewheel.prevent="handleScroll">
+    <h1>kakaka</h1>
+    <div class="project--div" ref="scrollDiv" >
       <div class="project--card">
-
+        1
       </div>
       <div class="project--card">
-
+        2
       </div>
       <div class="project--card">
-
+        3
       </div>
       <div class="project--card">
-
+        4
       </div>
       <div class="project--card">
-
+        5
       </div>
       <div class="project--card">
-
+        6
       </div>
       <div class="project--card">
-
+        7
+      </div>
+      <div class="project--card">
+        fin
       </div>
     </div>
   </section>
@@ -30,17 +34,13 @@
 export default {
   methods: {
     handleScroll(e) {
-      if (e.deltaY > 124){
-        this.$refs.scrollDiv.trnasform = translateX(100+'px');
+      if (e.deltaY > 0 ){
+        this.$refs.scrollDiv.scrollLeft += e.wheelDelta;
+      } else {
+        this.$refs.scrollDiv.scrollLeft += e.wheelDelta;
       }
     }
   },
-  mounted () {
-    window.addEventListener('mousewheel', this.handleScroll);
-  },
-  beforeDestroyed () {
-    window.removeEventListener('mousewheel', this.handleScroll);
-  }
 }
 </script>
 
@@ -48,13 +48,15 @@ export default {
 .container {
   background-color: #eee;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  justify-content: center;
   padding: 0;
   height: calc(100vh - 60px);
 }
 .project--div {
   display: flex;
   width: 100vw;
+  padding: 0 5vw;
   overflow-x: hidden;
 }
 .project--card {
@@ -62,7 +64,20 @@ export default {
   height: 12.36vw;
   background-color: yellow;
   border-radius: 15px;
-  margin-left: 5vw;
+  &:first-child {
+    margin-right: 5vw;
+  }
+  &:last-child {
+    min-width: 5vw;
+    width: 5vw;
+    margin-right: 5vw;
+    background-color: #181818;
+    color: #fff;
+  }
+  & + & {
+    margin-right: 5vw;
+  }
+  
 }
 
 </style>
